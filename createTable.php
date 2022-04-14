0caa2d1a13be55e0b5f3784610008d41c9a3271c
Md.Tahsinul Islam <mughdaw825@gmail.com>
2022-04-14 16:30:30 +0600

    Create createTable.php

---
+++ createTable.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "diuconnection";
+
+// I am Creating a connection here with MySQL.
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// I am Checking connection here. 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+// SQL query to creating a table in database.
+$sql = "CREATE TABLE usersinfo (
+  public_email VARCHAR(40) NOT NULL,
+  github VARCHAR(10),
+  bio VARCHAR(30),
+  twitter_username VARCHAR(40),
+  linkedIn_username VARCHAR(10),
+  facebook_username VARCHAR(30),
+  fileToUpload VARCHAR(100)
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table students created successfully";
+} else {
+  echo "Error! creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
